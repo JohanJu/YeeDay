8a84a236ce507a7dc1bf600aab2a08a7fe724c20
Johan Ju <johanj93@gmail.com>
2017-09-05 19:03:52 +0200

    init android app

---
+++ YeeApp/app/src/main/java/ya/yeeapp/OnOffTask.java
@@ -0,0 +1,18 @@
+package ya.yeeapp;
+
+import android.os.AsyncTask;
+import android.util.Log;
+import java.io.DataOutputStream;
+import java.net.Socket;
+class OnOffTask extends AsyncTask<Integer, Integer, Integer> {
+    protected Integer doInBackground(Integer... ints) {
+        return 0;
+    }
+
+    protected void onProgressUpdate(Integer... ints) {
+    }
+
+    protected void onPostExecute(Integer ints) {
+
+    }
+}
