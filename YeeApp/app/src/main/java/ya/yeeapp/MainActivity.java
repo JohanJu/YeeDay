75bdee18946b775a8df20da835c85f8e2552d9b7
Johan Ju <johanj93@gmail.com>
2017-09-14 23:52:52 +0200

    Part2 App

--- YeeApp/app/src/main/java/ya/yeeapp/MainActivity.java
+++ YeeApp/app/src/main/java/ya/yeeapp/MainActivity.java
@@ -5,13 +5,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,20 +20,46 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
+        editText = (EditText) findViewById(R.id.editText);
     }
-    public void on_off(View view) {
-        Log.e("test", "on_off");
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        editText.setText(udp("u").replaceAll("[^0-9]",""));
+    }
+
+    protected String udp(String msg){
+        Log.e("test", msg);
+        String re = "";
         try{
             DatagramSocket s = new DatagramSocket();
-            byte[] buf = "t".getBytes();
+            byte[] buf = msg.getBytes();
             InetAddress a = InetAddress.getByName("192.168.0.4");
             DatagramPacket p = new DatagramPacket(buf, buf.length, a, 23232);
             s.send(p);
-            Log.e("test", "connected");
+            if(msg.charAt(0) == 'u'){
+                s.setSoTimeout(1000);
+                buf = new byte[16];
+                DatagramPacket rp = new DatagramPacket(buf, buf.length);
+                s.receive(rp);
+                re = new String(rp.getData());
+            }
             s.close();
         }catch(Exception e){
             Log.e("test", "err", e);
         }
+        return re;
+    }
+    public void toggle(View view) {
+        udp("t");
+    }
+    public void movie(View view) {
+       udp("m");
+    }
+    public void alarm(View view) {
+        String time = editText.getText().toString();
+        udp("a"+time);
     }
 }
 
